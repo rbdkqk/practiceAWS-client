6b6e1a74aae2200838e38a87d2d9f92049dd9061
rbdkqk <rbdkqk@gmail.com>
2020-09-25 01:10:40 +0900

    Shortly AWS Deployment : 일단 다 된 것 같아서 올려둠
    1. cors 해결 : whitelist 방식을 활용해 낸 듯
      - https://github.com/codestates/help-desk/issues/2054
      - cors 와 origin 등 내용에 관한 공부가 필요하다.
    
    2. url을 ip가 아닌 `http://Shortly.com/D16fd9` 방식으로 표기해 주고 싶음.
      - src/components/LinkItem.js 파일을 수정하면 표기는 가능하나, 기능이 작동하지 않음.
      - 이건 서버의 도메인을 사야 해결되는 문제인 것 같다. (서버 코드의 app.js 참고)
    
    3. aws 서버 비밀번호의 환경변수에 관하여 고민해 보았음
      (1) aws 서버를 터미널로 열고, `export DATABASE_PASSWORD=` 명령어로 비밀번호를 줄 수 있다.
        - 그 다음에는 `node app.js`로 서버를 열든, `pm2 start app.js`로 열든 정상 작동함
        - 다만, node는 포트를 닫거나 터미널을 끄면 서버가 죽고 환경변수가 사라지는데,
        - pm2는 환경변수로 준 비밀번호가 살아있으니, pm2 기능에 따라, 터미널을 꺼도 그냥 계속 작동하는 듯?
      (2) pm2의 환경변수에 관하여 `ecosystem.config.js` 파일을 만들어 넣는 방법을 찾음
        - 다만, 제대로 사용할 방법을 모르겠고, 이 값을 이상하게 줘도 터미널에서 환경변수를 줘 놓으면 어차피 정상 작동함
        - ecosystem.config.js 파일은, 메뉴얼에 나온대로 사용하는 방법을 좀 배워야 하겠음
      (3) 그러면 pm2로 열면 내가 컴퓨터를 꺼도 포트가 안닫히고 환경변수도 그대로인 것 같은데,
        - '어떻게 하면 환경변수를 내가 매번 입력하지 않더라도 영속적으로 줄 수 있을까?'
    
    4. node든 pm2든 서버를 꺼도 포트가 여전히 열려 있는 경우가 매우 많음
      - 이것 때문에 `netstat -tnlp` 명령어와 `kill -9` 명령어를 매번 입력하고 있음
      - 방법이 없을까?
    
    5. 회원가입, 회원정보 등등의 기능이 구현되어 있지 않다.
      - shortly express - nightmare 스프린트를 약간 해 놓은 것을 여기에 쓸 수 있을지 생각해 볼 것.
      - nightmare에서도 info 부분은 안 한 것 같기는 하다.
    
    6. 기능의 작동 속도가 빠르지는 않다
      - 프리티어의 한계일까?

--- src/components/LinkItem.js
+++ src/components/LinkItem.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import "./LinkItem.css";
 
 const baseUrl = process.env.baseUrl || "3.20.232.121:13306";
+// const baseUrl = process.env.baseUrl || "Shortly.com";
+// 이렇게 바꿔줘야 페이지에 http://Shortly.com/D16fd9 이런식으로 표시된다.
+// 다만, 이러면 작동을 안함. 어딘가에서 손을 대야 한다.
+
+// 서버의 app.js 를 참고했는데,
+// 도메인을 사야 (위와 같이 ip로 나오지 않고) http://Shortly.com/D16fd9 방식으로 표현되는 것 같음..
 
 class LinkItem extends Component {
   render() {
